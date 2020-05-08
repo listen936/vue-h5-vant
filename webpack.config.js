01cb12c2f3eeb01b72c82163efd3a92937ab5308
lswang <lswang@speechx.cn>
2020-05-09 00:57:48 +0800

    feat(all): webpack之初体验

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin') // 注意：此写法与webpack中文官网上的不一致
+
+module.exports= {
+    entry:{
+        print:'./src/print.js',
+        app:'./src/index.js'
+    },
+    output:{
+        filename:'[name].bundle.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|gif|jpg|svg|jpeg)$/,
+                use:['file-loader']
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                use:['file-loader']
+            },
+            {
+                test:/\.(csv|tsv)$/,
+                use:['csv-loader']
+            },
+            {
+                test:/\.xml$/,
+                use:['xml-loader']
+            }
+        ]
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title:'good good study,day day up'
+        }),
+        
+    ],
+    devtool:'inline-source-map',
+    devServer:{
+        contentBase:'./dist'
+    }
+}
