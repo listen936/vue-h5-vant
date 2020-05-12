1c7c0bb88bcf22971bb97beba78096a4d9aaccc2
lswang <lswang@speechx.cn>
2020-05-13 01:14:57 +0800

    feat: vant按需引入

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
+
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="default">默认按钮</van-button>
+    <van-button color="#7232dd">单色按钮</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { Button } from 'vant'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    [Button.name]: Button
   }
 }
 </script>
