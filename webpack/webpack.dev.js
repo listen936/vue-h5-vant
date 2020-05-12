671f0023692060b9f34958e03f260339bf21dbb6
lswang <lswang@speechx.cn>
2020-05-13 00:06:51 +0800

    clear

--- webpack/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const common = require('./webpack.common')
-const merge = require('webpack-merge')
-
-module.exports = merge(common,{
-    devtool:'inline-source-map',
-    devServer:{
-        contentBase:'./dist'
-    }
-})
